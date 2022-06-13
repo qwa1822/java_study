17a564e0cb9dde68de3095d54c60917784805b66
qwa1822 <asdzxc1822@naver.com>
2022-06-13 16:13:36 +0900

    별찍기

--- src/study/ch04/ch4_13.java
+++ src/study/ch04/ch4_13.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class ch4_13 {
     public static void main(String[] args) {
 
-        int i;   //scope(범위)`
+        int i;   //scope(범위)  -선언위치부터 선언된 블럭의 끝까지
         
 
         for(i=1; i<=10; i++){   //괄호{} 안의 문장을 5번 반복
---
+++ src/study/ch04/ch4_15.java
@@ -0,0 +1,13 @@
+package study.ch04;
+
+public class ch4_15 {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=10; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
