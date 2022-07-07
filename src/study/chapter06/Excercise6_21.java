648f7707652c9ea89cbb4925db7321b910732806
qwa1822 <asdzxc1822@naver.com>
2022-07-07 11:06:59 +0900

    절대값

---
+++ src/study/chapter06/Excercise6_21.java
@@ -0,0 +1,19 @@
+package study.chapter06;
+
+public class Excercise6_21 {
+    public static int abs(int n){
+
+        return Math.abs(n);
+    }
+    public static void main(String[] args) {
+
+
+        int value=5;
+        System.out.println(value+"의 절대값"+abs(value));
+        value=10;
+        System.out.println(value+"의 절대값:"+abs(value));
+
+
+
+    }
+        }
