37903096955dfff65e5eca12b023404061e36481
qwa1822 <asdzxc1822@naver.com>
2022-08-01 14:46:59 +0900

    챕터5장 5-3

---
+++ src/ch06/exam.java
@@ -0,0 +1,16 @@
+package ch06;
+
+public class exam {
+    public static void main(String[] args) {
+
+
+        int[]arr={10,20,30,40,50};
+        int sum=0;
+
+        for(int i=0; i<arr.length; i++){
+            sum+=arr[i];
+        }
+
+        System.out.println(sum);
+    }
+}
